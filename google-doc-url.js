c8f3320113c7185ba10e507699a8a858ad1d1f28
dizzyizzy1 <113078164+dizzyizzy1@users.noreply.github.com>
2022-09-25 23:06:03 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1KqWCUoY0vGd61ghfWuH10mX_D_WSsdon0kL6E8oxkKE/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1KqWCUoY0vGd61ghfWuH10mX_D_WSsdon0kL6E8oxkKE/edit#gid=1033046795';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAYzwqMSuHYypP47WX71C0ykHONl5-QbPY';
